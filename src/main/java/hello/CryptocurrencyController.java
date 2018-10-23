1edd406bbdee14ebfc48b198e67c89ecdbe2d847
Marcelo Morgado <cmarcelom@gmail.com>
2018-10-23 22:07:38 +0100

    bug fix

--- src/main/java/hello/CryptocurrencyController.java
+++ src/main/java/hello/CryptocurrencyController.java
@@ -5,12 +5,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 class CryptocurrencyController {
 
     private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping(value = "/")
+    public List<Cryptocurrency> list() {
+        List<Cryptocurrency> list = new ArrayList<>();
+        list.add(new Cryptocurrency(1L, "Bitcoin"));
+        list.add(new Cryptocurrency(2L, "Ethereum"));
+        return list;
+    }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public Cryptocurrency create(@RequestParam(value="name", defaultValue="Bitcoin") String name) {
